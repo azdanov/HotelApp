83bdd8ae9feea20dcced17f21cad882703bc09c0
Anton Zdanov <anton@azdanov.dev>
2023-03-04 22:53:24 +0200

    add Bookings table

---
+++ HotelAppDB/dbo/Tables/Bookings.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Bookings]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[RoomId] INT NOT NULL,
+	[GuestId] INT NOT NULL,
+	[CheckedIn] BIT NOT NULL DEFAULT 0, 
+	[CheckInDate] DATETIME2 NOT NULL,
+	[CheckOutDate] DATETIME2 NOT NULL,
+	[TotalPrice] MONEY NOT NULL,
+    CONSTRAINT [FK_Bookings_Rooms] FOREIGN KEY ([RoomId]) REFERENCES [dbo].[Rooms] ([Id]),
+	CONSTRAINT [FK_Bookings_Guests] FOREIGN KEY ([GuestId]) REFERENCES [dbo].[Guests] ([Id])
+)
