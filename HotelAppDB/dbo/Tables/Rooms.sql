80c4865a3f4ae80bb60828aac67ab8294270c442
Anton Zdanov <anton@azdanov.dev>
2023-03-04 23:46:14 +0200

    add IsAvailable to Rooms

--- HotelAppDB/dbo/Tables/Rooms.sql
+++ HotelAppDB/dbo/Tables/Rooms.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
 	[RoomNumber] VARCHAR(10) NOT NULL, 
 	[RoomTypeId] INT NOT NULL, 
+	[IsAvailable] BIT NOT NULL DEFAULT 1,
 	CONSTRAINT [FK_Rooms_RoomTypes] FOREIGN KEY ([RoomTypeId]) REFERENCES [dbo].[RoomTypes] ([Id]) 
 )
